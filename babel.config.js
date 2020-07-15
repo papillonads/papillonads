3505059d273555cb3ceb4fd95546cf1b748033ff
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-15 11:36:13 +0200

    setup babel

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-catch-binding',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-classes',
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-transform-spread',
+    'syntax-async-functions',
+  ],
+}
