2ef54773195581fa7097c856baa14b97468960b3
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-15 11:58:36 +0200

    add library pagination

---
+++ packages/library/src/pagination/index.js
@@ -0,0 +1,9 @@
+export const paginationActionTypes = {
+  onChange: 'onChange',
+  onBackwards: 'onBackwards',
+  onForward: 'onForward',
+}
+
+export const pagination = {
+  paginationActionTypes,
+}
