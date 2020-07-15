cd9edf9ff5979b1ced99c28735fdab0be965c68d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-15 14:17:58 +0200

    add components content field group

---
+++ packages/components/src/content/FieldGroup/FieldGroup.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import cx from 'classnames'
+import { propTypes, defaultProps } from './FieldGroup.prop'
+import styles from './FieldGroup.scss'
+
+export function FieldGroup({ className, children, row, ...otherProps }) {
+  const classNames = cx(styles.FieldGroup, styles.className, {
+    [styles['FieldGroup--row']]: row,
+  })
+
+  return (
+    <div {...otherProps} className={classNames}>
+      {React.Children.map(children, (child) => (
+        <div className={styles.FieldGroup__item}>{child}</div>
+      ))}
+    </div>
+  )
+}
+
+FieldGroup.propTypes = propTypes
+
+FieldGroup.defaultProps = defaultProps
