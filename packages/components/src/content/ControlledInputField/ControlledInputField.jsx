defcaffd0a98d837d84f99f4da8369cbf37df995
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-15 14:17:06 +0200

    add components content controlled input field

---
+++ packages/components/src/content/ControlledInputField/ControlledInputField.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import cx from 'classnames'
+import { propTypes, defaultProps } from './ControlledInputField.prop'
+import styles from './ControlledInputField.scss'
+import { FormLabel } from '../FormLabel'
+import { HelpText } from '../HelpText'
+import { ValidationMessage } from '../ValidationMessage'
+import { ControlledInput } from '../ControlledInput'
+
+export function ControlledInputField({
+  className,
+  id,
+  labelIsLight,
+  required,
+  helpText,
+  disabled,
+  labelText,
+  helpTextProps,
+  formLabelProps,
+  checked,
+  value,
+  validationMessage,
+  onChange,
+  children,
+  inputType,
+  inputProps,
+  name,
+  ...otherProps
+}) {
+  const classNames = cx(styles.ControlledInputField, className, {
+    [styles['ControlledInputField--disabled']]: disabled,
+  })
+
+  return (
+    <div className={classNames} {...otherProps}>
+      <ControlledInput
+        id={id}
+        labelText={labelText}
+        type={inputType}
+        name={name}
+        required={required}
+        checked={checked}
+        disabled={disabled}
+        value={value}
+        onChange={onChange}
+        className={styles.ControlledInputField__input}
+        {...inputProps}
+      />
+      <div className={styles['Checkbox__label-wrapper']}>
+        <FormLabel
+          className={cx(styles.ControlledInputField__label, {
+            [styles['ControlledInputField__label--light']]: labelIsLight,
+          })}
+          required={required}
+          htmlFor={id}
+          {...formLabelProps}
+        >
+          {labelText}
+        </FormLabel>
+        {helpText && (
+          <HelpText className={styles['ControlledInputField__help-text']} {...helpTextProps}>
+            {helpText}
+          </HelpText>
+        )}
+        {validationMessage && (
+          <ValidationMessage className={styles['ControlledInputField__validation-message']}>{validationMessage}</ValidationMessage>
+        )}
+      </div>
+    </div>
+  )
+}
+
+ControlledInputField.propTypes = propTypes
+
+ControlledInputField.defaultProps = defaultProps
---
+++ packages/components/src/content/ControlledInputField/__tests__/ControlledInputField.int.render.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const ControlledInputField =
+  process.env.NODE_ENV === 'develop' || process.env.NODE_ENV === 'test'
+    ? require('../../../index').content.ControlledInputField
+    : require('../../../../build').content.ControlledInputField
+
+export function renderDefault() {
+  return <ControlledInputField id="checkbox" labelText="fantastic-label-text" />
+}
+
+export function renderWithAdditionalClassName() {
+  return <ControlledInputField className="fantastic-class-name" id="checkbox" labelText="fantastic-label-text" />
+}
+
+export function renderAsRequired() {
+  return <ControlledInputField className="fantastic-class-name" id="checkbox" labelText="fantastic-label-text" required />
+}
+
+export function renderWithHelpText() {
+  return <ControlledInputField className="fantastic-class-name" id="checkbox" labelText="fantastic-label-text" helpText="Help Text" />
+}
+
+export function renderWithValidationMessage() {
+  return (
+    <ControlledInputField className="fantastic-class-name" id="checkbox" labelText="fantastic-label-text" validationMessage="Not valid!" />
+  )
+}
+
+export function renderAsDisabled() {
+  return <ControlledInputField className="fantastic-class-name" id="checkbox" labelText="fantastic-label-text" disabled />
+}
+
+export function renderWithLightLabelVariation() {
+  return (
+    <ControlledInputField
+      className="fantastic-class-name"
+      id="checkbox"
+      labelText="fantastic-label-text"
+      validationMessage="Not valid!"
+      labelIsLight
+    />
+  )
+}
+
+export function renderAsChecked() {
+  return <ControlledInputField className="fantastic-class-name" id="checkbox" labelText="fantastic-label-text" checked />
+}
+
+export function renderWithValue() {
+  return <ControlledInputField className="fantastic-class-name" id="checkbox" labelText="fantastic-label-text" value="fantastic-value" />
+}
+
+export function renderWithNoA11yIsues() {
+  return <ControlledInputField id="checkbox" labelText="fantastic-label-text" />
+}
