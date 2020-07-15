6d183b9bf1a414db8fcc02700743b38ce1a0f922
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-15 11:51:01 +0200

    add docs index

---
+++ packages/docs/src/index.js
@@ -0,0 +1,20 @@
+/* eslint global-require: 0 */
+
+import './asset/npm/primer.css'
+import './asset/custom.css'
+import './asset/github/styleguide.css'
+import './asset/main.css'
+
+import './asset/github/styleguide'
+import './asset/npm/elasticlunr'
+import './asset/animation'
+// import './asset/search'
+
+require('@babel/register')({
+  extensions: ['.js', '.jsx'],
+  ignore: [],
+})
+
+require('whatwg-fetch')
+
+require('./clientRenderApp')
