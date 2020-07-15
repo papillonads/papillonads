9d0fbd6cd607ccffd9a35b6ba6c0e3109b8462b5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-15 14:11:07 +0200

    add components primer subhead

---
+++ packages/components/src/primer/Subhead/Subhead.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import cx from 'classnames'
+import { propTypes, defaultProps } from './Subhead.prop'
+import styles from './Subhead.scss'
+
+export function Subhead({ className, heading, isSpacious }) {
+  return (
+    <div
+      className={cx(className, styles.Subhead, {
+        [styles['Subhead--spacious']]: isSpacious === true,
+      })}
+    >
+      <div className={styles['Subhead-heading']}>{heading}</div>
+    </div>
+  )
+}
+
+Subhead.propTypes = propTypes
+
+Subhead.defaultProps = defaultProps
---
+++ packages/components/src/primer/Subhead/index.js
@@ -0,0 +1,1 @@
+export { Subhead } from './Subhead'
