2a9f1a3b7959b979453f1a9c999efbe4f6f745e6
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-15 11:59:00 +0200

    add library sort

---
+++ packages/library/src/sort/index.js
@@ -0,0 +1,15 @@
+export const sortOrder = {
+  ascending: 'ascending',
+  descending: 'descending',
+}
+
+export const sortDefault = {
+  index: 0,
+  value: 'id',
+  order: sortOrder.ascending,
+}
+
+export const sort = {
+  sortOrder,
+  sortDefault,
+}
