ca69bf9f5e9b74aa700cbd044c184b58bcd8a8c6
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-15 14:20:30 +0200

    add components content index

---
+++ packages/components/src/content/index.js
@@ -0,0 +1,53 @@
+import { Asset } from './Asset'
+import { AssetIcon } from './AssetIcon'
+import { Button } from './Button'
+import { Checkbox } from './Checkbox'
+import { CheckboxField } from './CheckboxField'
+import { ControlledInput } from './ControlledInput'
+import { ControlledInputField } from './ControlledInputField'
+import { CopyButton } from './CopyButton'
+import { EditorToolbar } from './EditorToolbar'
+import { EditorToolbarButton } from './EditorToolbarButton'
+import { FieldGroup } from './FieldGroup'
+import { FormLabel } from './FormLabel'
+import { HelpText } from './HelpText'
+import { Icon } from './Icon'
+import { IconButton } from './IconButton'
+import { Illustration } from './Illustration'
+import { InViewport } from './InViewport'
+import { Note } from './Note'
+import { RadioButton } from './RadioButton'
+import { RadioButtonField } from './RadioButtonField'
+import { SVG } from './SVG'
+import { Spinner } from './Spinner'
+import { TabFocusTrap } from './TabFocusTrap'
+import { TextLink } from './TextLink'
+import { ValidationMessage } from './ValidationMessage'
+
+export const content = {
+  Asset,
+  AssetIcon,
+  Button,
+  Checkbox,
+  CheckboxField,
+  ControlledInput,
+  ControlledInputField,
+  CopyButton,
+  EditorToolbar,
+  EditorToolbarButton,
+  FieldGroup,
+  FormLabel,
+  HelpText,
+  Icon,
+  IconButton,
+  Illustration,
+  InViewport,
+  Note,
+  RadioButton,
+  RadioButtonField,
+  SVG,
+  Spinner,
+  TabFocusTrap,
+  TextLink,
+  ValidationMessage,
+}
