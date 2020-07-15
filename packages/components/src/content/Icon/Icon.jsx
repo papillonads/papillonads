4ce21da8ba7dae45f34d43793451a42bcd0fe66b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-15 14:18:24 +0200

    add components content icon

---
+++ packages/components/src/content/Icon/Icon.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import cx from 'classnames'
+import { propTypes, defaultProps } from './Icon.prop'
+import { iconComponents } from './Icon.part'
+import styles from './Icon.scss'
+
+export function Icon({ className, icon, size, color, ...otherProps }) {
+  const classNames = cx(
+    styles.Icon,
+    {
+      [styles[`Icon--${size}`]]: size,
+      [styles[`Icon--${color}`]]: color,
+      [styles['Icon--trimmed']]: icon.toLowerCase().includes('trimmed'),
+    },
+    className,
+  )
+
+  const Element = iconComponents[icon]
+
+  return <Element className={classNames} {...otherProps} />
+}
+
+Icon.propTypes = propTypes
+
+Icon.defaultProps = defaultProps
