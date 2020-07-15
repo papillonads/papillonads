2d2f46ea935565db604de7bfbaae28a9d104dce3
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-15 14:16:50 +0200

    add components content checkbox field

---
+++ packages/components/src/content/CheckboxField/CheckboxField.prop.js
@@ -0,0 +1,9 @@
+import { propTypes as controlledInputFieldPropTypes } from '../ControlledInputField/ControlledInputField.prop'
+
+export const propTypes = controlledInputFieldPropTypes
+
+export const defaultProps = {
+  labelIsLight: false,
+  checked: false,
+  inputType: 'checkbox',
+}
