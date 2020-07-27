f4b32bba0c0d4018d717f08da0d62383ca238d47
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-27 22:17:32 +0200

    implement Microsite

---
+++ packages/components/src/primer/Concept/Microsite/Microsite.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { propTypes, defaultProps } from './Microsite.prop'
+
+export function SpecificationsStandardsGuidelines({ className }) {
+  return (
+    <div className={className}>
+      <h2>📗 Specifications/Standards/Guidelines</h2>
+      Coming soon...
+    </div>
+  )
+}
+
+SpecificationsStandardsGuidelines.propTypes = propTypes
+SpecificationsStandardsGuidelines.defaultProps = defaultProps
+
+export function OpenSourceDependencyProjects({ className }) {
+  return (
+    <div className={className}>
+      <h2>📗 Open Source Dependency Projects</h2>
+      Coming soon...
+    </div>
+  )
+}
+
+OpenSourceDependencyProjects.propTypes = propTypes
+OpenSourceDependencyProjects.defaultProps = defaultProps
+
+export function DesignSystemCore({ className }) {
+  return (
+    <div className={className}>
+      <h2>📗 Papillon Ads Implementation</h2>
+      <h3>📖. Design System Core</h3>
+      <h4>📃. Setup file(s)</h4>
+      <div>﹘ Coming soon.</div>
+      <h4>📃. Knowledge base</h4>
+      <div>﹘ Coming soon.</div>
+    </div>
+  )
+}
+
+DesignSystemCore.propTypes = propTypes
+DesignSystemCore.defaultProps = defaultProps
+
+export function ProductMicrosites({ className }) {
+  return (
+    <div className={className}>
+      <h2>📗 Papillon Ads Implementation</h2>
+      <h3>📖. Design System Core</h3>
+      <h4>📃. Setup file(s)</h4>
+      <div>﹘ Coming soon.</div>
+      <h4>📃. Knowledge base</h4>
+      <div>﹘ Coming soon.</div>
+    </div>
+  )
+}
+
+ProductMicrosites.propTypes = propTypes
+ProductMicrosites.defaultProps = defaultProps
+
+export const Microsite = {
+  SpecificationsStandardsGuidelines,
+  OpenSourceDependencyProjects,
+  Implementation: {
+    DesignSystemCore,
+    ProductMicrosites,
+  },
+}
---
+++ packages/components/src/primer/Concept/Microsite/index.js
@@ -0,0 +1,1 @@
+export { Microsite } from './Microsite'
