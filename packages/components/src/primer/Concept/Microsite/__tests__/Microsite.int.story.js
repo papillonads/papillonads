5d6851392f9e09a1b1529ea72b66edf22aca0da3
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-27 22:18:08 +0200

    story Microsite

---
+++ packages/components/src/primer/Concept/Microsite/__tests__/Microsite.int.story.js
@@ -0,0 +1,24 @@
+import { storiesOf } from '@storybook/react'
+import {
+  renderSpecificationsStandardsGuidelines,
+  renderOpenSourceDependencyProjects,
+  renderDesignSystemCore,
+  renderProductMicrosites,
+} from './Microsite.int.render'
+import notes from '../Microsite.md'
+
+storiesOf('Primer|Atom/Concept/Microsite', module)
+  .add('SpecificationsStandardsGuidelines', renderSpecificationsStandardsGuidelines, {
+    notes,
+  })
+  .add('OpenSourceDependencyProjects', renderOpenSourceDependencyProjects, {
+    notes,
+  })
+
+storiesOf('Primer|Atom/Concept/Microsite/Implementation', module)
+  .add('DesignSystemCore', renderDesignSystemCore, {
+    notes,
+  })
+  .add('ProductMicrosites', renderProductMicrosites, {
+    notes,
+  })
