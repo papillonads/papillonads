59b0e2e481511d7b61f3cddb45c6d94756edb2fe
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-27 23:19:43 +0200

    implement Routing

---
+++ packages/components/src/primer/Concept/Routing/Routing.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { propTypes, defaultProps } from './Routing.prop'
+
+export function SpecificationsStandardsGuidelines({ className }) {
+  return (
+    <div className={className}>
+      <h2>📗 Specifications/Standards/Guidelines</h2>
+      Coming soon...
+    </div>
+  )
+}
+
+SpecificationsStandardsGuidelines.propTypes = propTypes
+SpecificationsStandardsGuidelines.defaultProps = defaultProps
+
+export function OpenSourceDependencyProjects({ className }) {
+  return (
+    <div className={className}>
+      <h2>📗 Open Source Dependency Projects</h2>
+      <h3>📖. React Router</h3>
+      📝 Coming soon 🐬
+    </div>
+  )
+}
+
+OpenSourceDependencyProjects.propTypes = propTypes
+OpenSourceDependencyProjects.defaultProps = defaultProps
+
+export function DesignSystemCore({ className }) {
+  return (
+    <div className={className}>
+      <h2>📗 Papillon Ads Implementation</h2>
+      <h3>📖. Design System Core</h3>
+      <h4>📃. Setup file(s)</h4>
+      <div>﹘ Coming soon.</div>
+      <h4>📃. Knowledge base</h4>
+      <div>﹘ Coming soon.</div>
+    </div>
+  )
+}
+
+DesignSystemCore.propTypes = propTypes
+DesignSystemCore.defaultProps = defaultProps
+
+export function ProductMicrosites({ className }) {
+  return (
+    <div className={className}>
+      <h2>📗 Papillon Ads Implementation</h2>
+      <h3>📖. Product Microsites</h3>
+      <h4>📃. Setup file(s)</h4>
+      <div>﹘ Coming soon.</div>
+      <h4>📃. Knowledge base</h4>
+      <div>﹘ Coming soon.</div>
+    </div>
+  )
+}
+
+ProductMicrosites.propTypes = propTypes
+ProductMicrosites.defaultProps = defaultProps
+
+export const Routing = {
+  SpecificationsStandardsGuidelines,
+  OpenSourceDependencyProjects,
+  Implementation: {
+    DesignSystemCore,
+    ProductMicrosites,
+  },
+}
