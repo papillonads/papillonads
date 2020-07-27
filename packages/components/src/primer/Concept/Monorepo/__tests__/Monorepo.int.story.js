99d1f1da8f7b40e801820d7d330d7eaa1e319f19
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-27 22:39:24 +0200

    story Monorepo

---
+++ packages/components/src/primer/Concept/Monorepo/__tests__/Monorepo.int.story.js
@@ -0,0 +1,24 @@
+import { storiesOf } from '@storybook/react'
+import {
+  renderSpecificationsStandardsGuidelines,
+  renderOpenSourceDependencyProjects,
+  renderDesignSystemCore,
+  renderProductMicrosites,
+} from './Monorepo.int.render'
+import notes from '../Monorepo.md'
+
+storiesOf('Primer|Atom/Concept/Monorepo', module)
+  .add('SpecificationsStandardsGuidelines', renderSpecificationsStandardsGuidelines, {
+    notes,
+  })
+  .add('OpenSourceDependencyProjects', renderOpenSourceDependencyProjects, {
+    notes,
+  })
+
+storiesOf('Primer|Atom/Concept/Monorepo/Implementation', module)
+  .add('DesignSystemCore', renderDesignSystemCore, {
+    notes,
+  })
+  .add('ProductMicrosites', renderProductMicrosites, {
+    notes,
+  })
