73d1423b3d31033b3221d17aef619a3bf14d7087
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-27 22:08:12 +0200

    story ContinuousDelivery

---
+++ packages/components/src/primer/Concept/ContinuousDelivery/__tests__/ContinuousDelivery.int.story.js
@@ -0,0 +1,24 @@
+import { storiesOf } from '@storybook/react'
+import {
+  renderSpecificationsStandardsGuidelines,
+  renderOpenSourceDependencyProjects,
+  renderDesignSystemCore,
+  renderProductMicrosites,
+} from './ContinuousDelivery.int.render'
+import notes from '../ContinuousDelivery.md'
+
+storiesOf('Primer|Atom/Concept/ContinuousDelivery', module)
+  .add('SpecificationsStandardsGuidelines', renderSpecificationsStandardsGuidelines, {
+    notes,
+  })
+  .add('OpenSourceDependencyProjects', renderOpenSourceDependencyProjects, {
+    notes,
+  })
+
+storiesOf('Primer|Atom/Concept/ContinuousDelivery/Implementation', module)
+  .add('DesignSystemCore', renderDesignSystemCore, {
+    notes,
+  })
+  .add('ProductMicrosites', renderProductMicrosites, {
+    notes,
+  })
