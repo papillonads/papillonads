6823a6ae3577cf18f66b1487b9d9bdf35c126b34
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-07-27 23:20:16 +0200

    test Routing

---
+++ packages/components/src/primer/Concept/Routing/__tests__/Routing.int.test.js
@@ -0,0 +1,23 @@
+import {
+  renderSpecificationsStandardsGuidelines,
+  renderOpenSourceDependencyProjects,
+  renderDesignSystemCore,
+  renderProductMicrosites,
+} from './Routing.int.render'
+
+describe('<Routing />', () => {
+  describe('Render', () => {
+    test('must match renderSpecificationsStandardsGuidelines()', () => {
+      expect(global.renderToJSON(renderSpecificationsStandardsGuidelines())).toMatchSnapshot()
+    })
+    test('must match renderOpenSourceDependencyProjects()', () => {
+      expect(global.renderToJSON(renderOpenSourceDependencyProjects())).toMatchSnapshot()
+    })
+    test('must match renderDesignSystemCore()', () => {
+      expect(global.renderToJSON(renderDesignSystemCore())).toMatchSnapshot()
+    })
+    test('must match renderProductMicrosites()', () => {
+      expect(global.renderToJSON(renderProductMicrosites())).toMatchSnapshot()
+    })
+  })
+})
