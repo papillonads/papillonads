3195fcef3574ef62f1d1adb63cf0a92da20fa997
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-08-04 17:50:44 +0200

    implement Concept LocalDevelopment documentation component

--- packages/components/src/primer/Concept/LocalDevelopment/LocalDevelopment.jsx
+++ packages/components/src/primer/Concept/LocalDevelopment/LocalDevelopment.jsx
@@ -5,6 +5,7 @@ export function SpecificationsStandardsGuidelines({ className }) {
   return (
     <div className={className}>
       <h2>📗 Specifications/Standards/Guidelines</h2>
+      <div>📝. Coming soon 🐬</div>
     </div>
   )
 }
@@ -16,6 +17,18 @@ export function OpenSourceDependencyProjects({ className }) {
   return (
     <div className={className}>
       <h2>📗 Open Source Dependency Projects</h2>
+      <br />
+      <br />
+      <h3>📖. Webpack DevServer</h3>
+      <br />
+      <div>
+        ﹘ <a href="https://github.com/webpack/webpack-dev-server">Webpack DevServer in Webpack</a> Serves a webpack app. Updates the
+        browser on changes
+      </div>
+      <div>
+        ﹘ <a href="https://webpack.js.org/configuration/dev-server/"> Webpack DevServer in Webpack Docs</a> webpack-dev-server can be used
+        to quickly develop an application. See the development guide to get started
+      </div>
     </div>
   )
 }
@@ -27,6 +40,11 @@ export function DesignSystemCore({ className }) {
   return (
     <div className={className}>
       <h2>📗 Papillon Ads Implementation</h2>
+      <h3>📖. Design System Core</h3>
+      <h4>📃. Setup file(s)</h4>
+      <div>﹘ Coming soon.</div>
+      <h4>📃. Knowledge base</h4>
+      <div>﹘ Coming soon.</div>
     </div>
   )
 }
@@ -38,6 +56,11 @@ export function ProductMicrosites({ className }) {
   return (
     <div className={className}>
       <h2>📗 Papillon Ads Implementation</h2>
+      <h3>📖. Product Microsites</h3>
+      <h4>📃. Setup file(s)</h4>
+      <div>﹘ Coming soon.</div>
+      <h4>📃. Knowledge base</h4>
+      <div>﹘ Coming soon.</div>
     </div>
   )
 }
