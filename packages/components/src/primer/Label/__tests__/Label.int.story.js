b4ce1a8f88e599736cc76b25b2e1f5146e0b9676
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-13 13:48:41 +0100

    update primer Label

--- packages/components/src/primer/Label/__tests__/Label.int.story.js
+++ packages/components/src/primer/Label/__tests__/Label.int.story.js
@@ -1,5 +1,20 @@
-import { storiesOf } from '@storybook/react'
-import { renderDefault } from './Label.int.render'
-import notes from '../Label.md'
+import React from 'react'
+import { withTests } from '@storybook/addon-jest'
+import results from '../../../../../../.jest-test-results.json'
 
-storiesOf('Primer|Atom/Label', module).add('Default', renderDefault, { notes })
+const Label =
+  process.env.NODE_ENV === 'develop' || process.env.NODE_ENV === 'test'
+    ? require('../../../index').primer.Label
+    : require('../../../../build').primer.Label
+
+export default {
+  title: 'Primer/Atom/Label',
+  component: Label,
+  decorators: [withTests({ results })],
+  parameters: { jest: ['Label.int.test.js'] },
+  excludeStories: ['custom'],
+}
+
+export function regular() {
+  return <Label text="default label" />
+}
