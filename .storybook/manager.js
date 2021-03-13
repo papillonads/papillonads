838308b51f26d7445ba95fe7abd217c4fb57f6e4
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-13 13:41:12 +0100

    setup storybook

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,12 @@
 import { addons } from '@storybook/addons'
-import { create } from '@storybook/theming/create'
-import { designSystemTheme } from './designSystemTheme'
+import { create } from '@storybook/theming'
 
 addons.setConfig({
-  theme: designSystemTheme,
+  panelPosition: 'right',
+  theme: create({
+    base: 'light',
+    brandTitle: 'Papillon Ads Components',
+    brandUrl: 'https://github.com/papillonads/papillonads',
+    brandImage: 'https://avatars2.githubusercontent.com/u/47340326',
+  }),
 })
