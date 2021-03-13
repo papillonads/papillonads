5118400d3f67df67938c159df4c3b0b06feb8669
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-13 17:58:48 +0100

    align strip ids with readme

--- packages/components/src/concept/Concepts/Concepts.part.js
+++ packages/components/src/concept/Concepts/Concepts.part.js
@@ -4,8 +4,6 @@ export const stripIds = [
   'badges',
   'logo',
   'heading',
-  'status',
-  'status-quote',
   'intro',
   'intro-quote',
   'packages',
@@ -34,7 +32,9 @@ export const stripIds = [
   'version-control-implementation',
   'components-package',
   'css-package',
+  'docs-package',
   'library-package',
+  'styleguide-package',
 ]
 
 export function createElement(tag, innerHTML) {
