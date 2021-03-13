5ac1b0f123f79e458e70868e0ec58461ee9aa29b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-13 14:33:01 +0100

    update library date

--- packages/library/src/date/index.js
+++ packages/library/src/date/index.js
@@ -1,7 +1,7 @@
-import { getLocaleDateTimeString } from './locale'
-import { getRandomDate } from './random'
+export function getLocaleDateTimeString(date) {
+  return date ? `${date.toLocaleDateString()} ${date.toLocaleTimeString()}` : null
+}
 
-export const date = {
-  getLocaleDateTimeString,
-  getRandomDate,
+export function getRandomDate(start = new Date(2013, 1, 1), end = new Date()) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
 }
