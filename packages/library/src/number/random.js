79a2767186916b2d49fe8a0230b0d846874ab2f4
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-13 14:33:56 +0100

    update library random

--- packages/library/src/number/random.js
+++
@@ -1,7 +0,0 @@
-export function getRandomDecimal({ min = 0, max, decimal = 2 }) {
-  return (Math.floor(Math.random() * (max - min) + min) / 100).toFixed(decimal)
-}
-
-export function getRandomInteger({ min = 0, max }) {
-  return Math.floor(Math.random() * (max - min) + min)
-}
