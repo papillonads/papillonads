b04396f36cfdba9140604fa516bbaad941b0e984
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-13 13:44:34 +0100

    update primer Blankslate

--- packages/components/src/primer/Blankslate/Blankslate.md
+++ packages/components/src/primer/Blankslate/Blankslate.md
@@ -1,39 +1,57 @@
 # Blankslate
 
-- Default
-- With Octicons
-- Narrow
-- Capped
-- Spacious
-- Large
-- No background
-
 > Blankslates are for when there is a lack of content within a page or section. Use them as placeholders to tell users why something isn't there. Be sure to provide an action to add content as well.
+
+# Variants
+
+> - Default
+> - Narrow, Capped, Spacious
+> - Large, No background
 
 ## Default
 
-Wrap some content in the outer .blankslate wrapper to give it the blankslate appearance.
+> Wrap some content in the outer .blankslate wrapper to give it the blankslate appearance.
 
-## With Octicons
-
-When it helps the message, include (relevant) icons in your blank slate. Add .blankslate-icon to any .mega-octicons as the first elements in the blankslate, like so.
+```jsx
+<Blankslate variant={blankslateVariant.default} heading={heading} text={text} />
+```
 
 ## Narrow
 
-Narrows the blankslate container to not occupy the entire available width.
+> Narrows the blankslate container to not occupy the entire available width.
+
+```jsx
+<Blankslate variant={blankslateVariant.narrow} heading={heading} text={text} />
+```
 
 ## Capped
 
-Removes the border-radius on the top corners.
+> Removes the border-radius on the top corners.
+
+```jsx
+<Blankslate variant={blankslateVariant.capped} heading={heading} text={text} />
+```
 
 ## Spacious
 
-Significantly increases the vertical padding.
+> Significantly increases the vertical padding.
+
+```jsx
+<Blankslate variant={blankslateVariant.spacious} heading={heading} text={text} />
+```
 
 ## Large
 
-Increases the size of the text in the blankslate
+> Increases the size of the text in the blankslate
+
+```jsx
+<Blankslate variant={blankslateVariant.large} heading={heading} text={text} />
+```
 
 ## No background
 
-Removes the background-color and border.
+> Removes the background-color and border.
+
+```jsx
+<Blankslate hasCleanBackground={true} heading={heading} text={text} />
+```
--- packages/components/src/primer/Blankslate/__tests__/Blankslate.int.story.js
+++ packages/components/src/primer/Blankslate/__tests__/Blankslate.int.story.js
@@ -1,11 +1,44 @@
-import { storiesOf } from '@storybook/react'
-import { renderDefault, renderNarrow, renderCapped, renderSpacious, renderLarge, renderCleanBackground } from './Blankslate.int.render'
-import notes from '../Blankslate.md'
+import React from 'react'
+import { withTests } from '@storybook/addon-jest'
+import { blankslateVariant } from '../Blankslate.prop'
+import results from '../../../../../../.jest-test-results.json'
 
-storiesOf('Primer|Atom/Blankslate', module)
-  .add('Default', renderDefault, { notes })
-  .add('Narrow', renderNarrow, { notes })
-  .add('Capped', renderCapped, { notes })
-  .add('Spacious', renderSpacious, { notes })
-  .add('Large', renderLarge, { notes })
-  .add('CleanBackground', renderCleanBackground, { notes })
+const Blankslate =
+  process.env.NODE_ENV === 'develop' || process.env.NODE_ENV === 'test'
+    ? require('../../../index').primer.Blankslate.Blankslate
+    : require('../../../../build').primer.Blankslate.Blankslate
+
+const heading = 'This is a blank slate'
+const text = 'Use it to provide information when no dynamic content exists.'
+
+export default {
+  title: 'Primer/Atom/Blankslate',
+  component: Blankslate,
+  decorators: [withTests({ results })],
+  parameters: { jest: ['Blankslate.int.test.js'] },
+  excludeStories: ['custom'],
+}
+
+export function regular() {
+  return <Blankslate variant={blankslateVariant.default} heading={heading} text={text} />
+}
+
+export function narrow() {
+  return <Blankslate variant={blankslateVariant.narrow} heading={heading} text={text} />
+}
+
+export function capped() {
+  return <Blankslate variant={blankslateVariant.capped} heading={heading} text={text} />
+}
+
+export function spacious() {
+  return <Blankslate variant={blankslateVariant.spacious} heading={heading} text={text} />
+}
+
+export function large() {
+  return <Blankslate variant={blankslateVariant.large} heading={heading} text={text} />
+}
+
+export function cleanBackground() {
+  return <Blankslate hasCleanBackground={true} heading={heading} text={text} />
+}
