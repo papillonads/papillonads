1950e59e1c5557f7c67d5d12742241dd44fd2d2c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-13 13:58:42 +0100

    update primer Concept ComponentDrivenDevelopment

--- packages/components/src/primer/Concept/ComponentDrivenDevelopment/__tests__/ComponentDrivenDevelopment.int.story.js
+++ packages/components/src/primer/Concept/ComponentDrivenDevelopment/__tests__/ComponentDrivenDevelopment.int.story.js
@@ -1,24 +1,32 @@
-import { storiesOf } from '@storybook/react'
-import {
-  renderSpecificationsStandardsGuidelines,
-  renderOpenSourceDependencyProjects,
-  renderDesignSystemCore,
-  renderProductMicrosites,
-} from './ComponentDrivenDevelopment.int.render'
-import notes from '../ComponentDrivenDevelopment.md'
+import React from 'react'
+import { withTests } from '@storybook/addon-jest'
+import results from '../../../../../../../.jest-test-results.json'
 
-storiesOf('Primer|Atom/Concept/ComponentDrivenDevelopment', module)
-  .add('SpecificationsStandardsGuidelines', renderSpecificationsStandardsGuidelines, {
-    notes,
-  })
-  .add('OpenSourceDependencyProjects', renderOpenSourceDependencyProjects, {
-    notes,
-  })
+const ComponentDrivenDevelopment =
+  process.env.NODE_ENV === 'develop' || process.env.NODE_ENV === 'test'
+    ? require('../../../../index').primer.Concept.ComponentDrivenDevelopment
+    : require('../../../../../build').primer.Concept.ComponentDrivenDevelopment
 
-storiesOf('Primer|Atom/Concept/ComponentDrivenDevelopment/Implementation', module)
-  .add('DesignSystemCore', renderDesignSystemCore, {
-    notes,
-  })
-  .add('ProductMicrosites', renderProductMicrosites, {
-    notes,
-  })
+export default {
+  title: 'Primer/Template/Concept/ComponentDrivenDevelopment',
+  component: ComponentDrivenDevelopment,
+  decorators: [withTests({ results })],
+  parameters: { jest: ['ComponentDrivenDevelopment.int.test.js'] },
+  excludeStories: ['custom'],
+}
+
+export function specificationsStandardsGuidelines() {
+  return <ComponentDrivenDevelopment.SpecificationsStandardsGuidelines />
+}
+
+export function openSourceDependencyProjects() {
+  return <ComponentDrivenDevelopment.OpenSourceDependencyProjects />
+}
+
+export function designSystemCore() {
+  return <ComponentDrivenDevelopment.Implementation.DesignSystemCore />
+}
+
+export function productMicrosites() {
+  return <ComponentDrivenDevelopment.Implementation.ProductMicrosites />
+}
