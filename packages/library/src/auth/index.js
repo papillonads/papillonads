17fafb44ca7728f3b56b87ed276dc13351cec52f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-13 14:32:37 +0100

    update library auth

---
+++ packages/library/src/auth/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Octokit } from '@octokit/rest'
+
+export function NotAuthorized() {
+  return <h1>You are not authorized to proceed!</h1>
+}
+
+export async function isUserAuthenticatedPapillonAdsOrganizationMember(personalAccessToken) {
+  let collectedAccessToken = personalAccessToken
+
+  if (!collectedAccessToken) {
+    collectedAccessToken = window.prompt('Please authenticate with your personal token') // eslint-disable-line
+  }
+
+  if (!collectedAccessToken) {
+    return null
+  }
+
+  const octokit = new Octokit({ auth: collectedAccessToken })
+
+  try {
+    // https://developer.github.com/v3/orgs/members/#list-organization-memberships-for-the-authenticated-user
+    const { data } = await octokit.request('GET /user/memberships/orgs')
+
+    const isActivePapillonAdsMember =
+      data.filter(
+        (membership) =>
+          membership.organization.login === 'papillonads' &&
+          membership.organization_url === 'https://api.github.com/orgs/papillonads' &&
+          membership.role === 'admin' &&
+          membership.state === 'active',
+      ).length > 0
+
+    return isActivePapillonAdsMember
+  } catch (error) {
+    return false
+  }
+}
