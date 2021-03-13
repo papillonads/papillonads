902195b15c7556ca6c5943d6287c40896bf78596
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-13 14:34:29 +0100

    update library sort

--- packages/library/src/sort/index.js
+++ packages/library/src/sort/index.js
@@ -9,7 +9,70 @@ export const sortDefault = {
   order: sortOrder.ascending,
 }
 
-export const sort = {
-  sortOrder,
-  sortDefault,
+export function getSort({ currentSort, newSort }) {
+  return {
+    index: newSort.index,
+    value: newSort.value,
+    order: (() => {
+      if (currentSort === sortDefault) {
+        return sortDefault.order
+      }
+      if (currentSort.index === newSort.index) {
+        return currentSort.order === sortOrder.ascending ? sortOrder.descending : sortOrder.ascending
+      }
+      return sortDefault.order
+    })(),
+  }
+}
+
+export function sortObjects({ sort, objects }) {
+  if (!objects) {
+    return objects
+  }
+
+  if (sort.order === sortOrder.ascending) {
+    return objects.sort((a, b) => {
+      switch (typeof a?.[sort.value]) {
+        case 'object':
+          if (a?.[sort.value] instanceof Object && a?.[sort.value] instanceof Date) {
+            return a?.[sort.value] > b?.[sort.value] ? 1 : -1
+          }
+          if (a?.[sort.value] instanceof Object && a?.[sort.value] instanceof Array) {
+            return a?.[sort.value]?.find((item) => item.isSelected === true)?.text?.toLowerCase() >
+              b?.[sort.value]?.find((item) => item.isSelected === true)?.text?.toLowerCase()
+              ? 1
+              : -1
+          }
+          return a?.[sort.value]?.value?.toLowerCase() > b?.[sort.value]?.value?.toLowerCase() ? 1 : -1
+        case 'string':
+          return a?.[sort.value]?.toLowerCase() > b?.[sort.value]?.toLowerCase() ? 1 : -1
+        case 'number':
+          return a?.[sort.value] > b?.[sort.value] ? 1 : -1
+        default:
+          return -1
+      }
+    })
+  }
+
+  return objects.sort((a, b) => {
+    switch (typeof a?.[sort.value]) {
+      case 'object':
+        if (a?.[sort.value] instanceof Object && a?.[sort.value] instanceof Date) {
+          return a?.[sort.value] < b?.[sort.value] ? 1 : -1
+        }
+        if (a?.[sort.value] instanceof Object && a?.[sort.value] instanceof Array) {
+          return a?.[sort.value]?.find((item) => item.isSelected === true)?.text?.toLowerCase() <
+            b?.[sort.value]?.find((item) => item.isSelected === true)?.text?.toLowerCase()
+            ? 1
+            : -1
+        }
+        return a?.[sort.value]?.value?.toLowerCase() < b?.[sort.value]?.value?.toLowerCase() ? 1 : -1
+      case 'string':
+        return a?.[sort.value]?.toLowerCase() < b?.[sort.value]?.toLowerCase() ? 1 : -1
+      case 'number':
+        return a?.[sort.value] < b?.[sort.value] ? 1 : -1
+      default:
+        return -1
+    }
+  })
 }
