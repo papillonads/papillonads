7966e2274b22940e98ab27101bbc181b83a12ec5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-28 22:57:20 +0200

    update webpack setup styleguide

--- packages/styleguide/webpack.common.js
+++ packages/styleguide/webpack.common.js
@@ -44,11 +44,18 @@ const configureBabelLoader = (browserList) => {
         ],
         plugins: [
           '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-proposal-nullish-coalescing-operator',
           '@babel/plugin-proposal-object-rest-spread',
           '@babel/plugin-proposal-optional-catch-binding',
           '@babel/plugin-proposal-optional-chaining',
           '@babel/plugin-syntax-dynamic-import',
           '@babel/plugin-transform-classes',
+          [
+            '@babel/plugin-transform-react-jsx',
+            {
+              runtime: 'automatic',
+            },
+          ],
           '@babel/plugin-transform-runtime',
           '@babel/plugin-transform-spread',
           'syntax-async-functions',
