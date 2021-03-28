b35c803a8787344c89312305c3abe46d27c954fc
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-28 23:47:57 +0200

    lint and format components content AssetIcon

--- packages/components/src/content/AssetIcon/AssetIcon.jsx
+++ packages/components/src/content/AssetIcon/AssetIcon.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { propTypes, defaultProps } from './AssetIcon.prop'
 import { Illustration } from '../Illustration'
 import { illustrationName } from '../Illustration/Illustration.prop'
