0ef55b15b96844e14a1c0eaecef121c9067444ef
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-08-30 10:11:09 +0200

    bumpstack papillonads

--- packages/docs/webpack.common.js
+++ packages/docs/webpack.common.js
@@ -34,7 +34,7 @@ const configureBabelLoader = (browserList) => {
             {
               modules: false,
               useBuiltIns: 'entry',
-              corejs: '3.16.1',
+              corejs: '3.16.4',
               targets: {
                 browsers: browserList,
               },
--- packages/styleguide/bumpstack.sh
+++ packages/styleguide/bumpstack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-npm un -D nodemon webpack webpack-dev-server && npm i -D nodemon webpack@^4.46.0 webpack-dev-server
+npm un -D nodemon webpack webpack-dev-server && npm i -D nodemon webpack@^4.46.0 webpack-dev-server@^3.11.2
 
 npm run clean-up && npm run install-packages && npm audit fix
