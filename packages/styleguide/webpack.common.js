545ea8419c46850bf6f45c8c8ef5df75f595ceef
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-09-13 16:27:11 +0200

    bumpstack papillonads

--- packages/styleguide/webpack.common.js
+++ packages/styleguide/webpack.common.js
@@ -34,7 +34,7 @@ const configureBabelLoader = (browserList) => {
             {
               modules: false,
               useBuiltIns: 'entry',
-              corejs: '3.25.0',
+              corejs: '3.25.1',
               targets: {
                 browsers: browserList,
               },
