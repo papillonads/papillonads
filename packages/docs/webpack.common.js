5c95c0c4fee6bae0c4a2a2d14ea6caed201aacda
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-09-25 11:55:06 +0200

    bumpstack papillonads comply with node v14.20.1 and npm v6.14.17

--- packages/docs/webpack.common.js
+++ packages/docs/webpack.common.js
@@ -34,7 +34,7 @@ const configureBabelLoader = (browserList) => {
             {
               modules: false,
               useBuiltIns: 'entry',
-              corejs: '3.25.1',
+              corejs: '3.25.2',
               targets: {
                 browsers: browserList,
               },
